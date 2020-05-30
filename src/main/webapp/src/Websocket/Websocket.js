e67d1b99749cfec509cc973f34ab616a8ed76425
Sander Siim <sander.siim1@gmail.com>
2020-05-30 15:38:50 +0300

    Frontend cleanup

--- src/main/webapp/src/Websocket/Websocket.js
+++ src/main/webapp/src/Websocket/Websocket.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import SockJsClient from 'react-stomp';
 
-class Websocket extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
+class Websocket extends PureComponent {
     sendMessage = (msg) => {
         this.clientRef.sendMessage('/app/register', msg);
     };
 
-    render() {
-        return (
-            <div>
-                <SockJsClient url='http://localhost:8080/secrethit' topics={['/topic/registrations']}
-                              onMessage={(msg) => { console.log(msg); }}
-                              ref={ (client) => { this.clientRef = client }} />
-            </div>
-        );
-    }
+    render = () => (
+        <SockJsClient
+            url='http://localhost:8080/secrethit'
+            topics={['/topic/registrations']}
+            onMessage={(msg) => { console.log(msg); }}
+            ref={(client) => { this.clientRef = client }}
+        />
+    )
 }
 
 export default Websocket;
